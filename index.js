c313dd897d6013e77fe6df4ff1e31cc797b7a661
Kwame Okai Addo <48158461+Kwame-Addo@users.noreply.github.com>
2022-12-10 07:46:22 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+
 jQuery(document).ready(function($){
 	var $timeline_block = $('.cd-timeline-block');
 
 	//hide timeline blocks which are outside the viewport
+    
 	$timeline_block.each(function(){
-		if($(this).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
+		if($(this).offset().top >
+           
+           $(window).scrollTop()+$(window).height()*0.75) {
 			$(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
 		}
 	});
